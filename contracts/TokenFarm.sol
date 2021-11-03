b80e5cf2100ddc849d4c11f1c3656d8dbd2f0a38
cromewar <vasiliy@cromewar.com>
2021-11-03 08:26:50 -0500

    basic token functionallity

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -13,22 +13,69 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenFarm is Ownable {
+    //mapping token address -> staker address -> amount
+    mapping(address => mapping(address => uint256)) public stakingBalance;
+
+    //mapping address -> uint256
+    mapping(address => uint256) public uniqueTokensStaked;
+
+    //Array for the allowed token addresses
     address[] public allowedTokens;
 
-    function stakeTokens(uint256 _amount, address _token) public {
-        // What tokens can users stake?
-        // how much can the users stake?
+    //Array for all the stakers
+    address[] public stakers;
 
-        require(_amount > 0, "Amount must be more than 0");
-        require(tokenIsAllowed(_token), "Token is currenty not allowed");
-        // Call transferFrom (ERC20)
+    // reward the user fot stake them tokens
+    function issueTokens() public onlyOwner {
+        // needs a list of all the stakers
     }
 
-    function addAllowdTokens(address _token) public onlyOwner {
+    function stakeTokens(uint256 _amount, address _token) public {
+        // what token can they stake?
+        // how much can they stake?
+        require(_amount > 0, "Amount must be more tha 0");
+        // require the token is on the allowed list
+        require(
+            tokenIsAllowed(_token),
+            "This token is not allowed at the moment"
+        );
+        // transferFrom (IERC20 Token standard from openzeppelin)
+        // transferFrom takes: address sender, address recipient, uint256 amount
+        // we send from the user account address to this contract.
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        // we need to track the amount of the tokens the user send
+        // how many unique tokens the user has?
+        updateUniqueTokensStaked(msg.sender, _token);
+        // add to whatever the user already had before the new staked amount
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
+
+        // check if is the first time the staker add funds to the pool and add it to the stakers list
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
+    }
+
+    function updateUniqueTokensStaked(address _user, address _token) internal {
+        // have an idea about how many unique tokens the user has
+        // if the user has one unique token is added to the list,
+        // if has more means them already been added
+
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
+    }
+
+    // Add allowed tokens to the list (just the owner should do this)
+    function addAllowedToken(address _token) public onlyOwner {
         allowedTokens.push(_token);
     }
 
+    //Verify if the token is on the allowed list
+
     function tokenIsAllowed(address _token) public view returns (bool) {
+        // loop the allowedTokens list a look if the token is there
         for (
             uint256 allowedTokensIndex = 0;
             allowedTokensIndex < allowedTokens.length;
--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -5,13 +5,13 @@ from brownie import TokenFarm
 def test_can_add_token():
     account = get_account()
     token_farm = TokenFarm.deploy({"from": account})
-    token_farm.addAllowdTokens(account.address)
+    token_farm.addAllowedToken(account.address)
     assert token_farm.allowedTokens(0) == account.address
 
 
 def test_token_is_allowed():
     account = get_account()
     token_farm = TokenFarm.deploy({"from": account})
-    token_farm.addAllowdTokens(account.address)
+    token_farm.addAllowedToken(account.address)
     tx = token_farm.tokenIsAllowed(get_account(index=2))
     assert tx == False
