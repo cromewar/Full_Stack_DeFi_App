5fbe7cccc4824aeb1c29c864b3aed1500ad8b236
cromewar <vasiliy@cromewar.com>
2021-11-09 21:00:46 -0500

    adding staking functionallity to the fron-end

--- front_end/src/components/yourWallet/StakeForm.tsx
+++ front_end/src/components/yourWallet/StakeForm.tsx
@@ -2,22 +2,53 @@ import { Token } from "../Main"
 import { useEthers, useTokenBalance } from "@usedapp/core"
 import { formatUnits } from "@ethersproject/units"
 import Box from '@mui/material/Box'
-import { Button } from '@mui/material';
+import { Button, Input } from '@mui/material';
+import React, { useState } from "react"
+import { useStakeTokens } from "../../hooks/useStakeTokens"
+import { utils } from "ethers"
 
 interface StakeFormProps {
     token: Token
 }
 
 export const StakeForm = ({ token }: StakeFormProps) => {
+
+
+    const sx = {
+        container: {
+            margin: 10,
+
+        }
+    }
+
     const { address: tokenAddres, name } = token
     const { account } = useEthers()
     const tokenBalance = useTokenBalance(tokenAddres, account)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
+    const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newAmount = event.target.value === "" ? "" : Number(event.target.value)
+        setAmount(newAmount)
+
+    }
+
+    const { approveAndStake, approveErc20State } = useStakeTokens(tokenAddres)
+
+    const handleStakeSubmit = () => {
+        const amountAsWei = utils.parseEther(amount.toString())
+        return approveAndStake(amountAsWei.toString())
+    }
 
     return (
-        <Box component="div">
-            <Button variant="contained" color="secondary" size="large">Stake!!</Button>
-        </Box>
+        <>
+            <Input
+                onChange={handleInputChange}
+            />
+            <Button variant="contained" color="secondary" size="large" style={sx.container}
+                onClick={handleStakeSubmit}>Stake!!</Button>
+        </>
+
     )
 
 }
---
+++ front_end/src/hooks/useStakeTokens.ts
@@ -0,0 +1,49 @@
+import {useContractFunction, useEthers} from '@usedapp/core'
+import TokenFarm from "../chain-info/contracts/TokenFarm.json"
+import ERC20 from "../chain-info/contracts/MockERC20.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import {constants, utils} from "ethers"
+// import {Contract} from '@ethersproject/contracts'
+import {Contract} from '@usedapp/core/node_modules/@ethersproject/contracts'
+import {useEffect, useState} from "react"
+
+export const useStakeTokens = (tokenAddress: string) => {
+    // chainId 
+    const {chainId} = useEthers()
+    // abi
+    const {abi} = TokenFarm
+    // address
+    // const dappTokenAddress = chainId ? networkMapping[String(chainId)]["DappToken"][0] : constants.AddressZero
+    const tokenFarmAddress = chainId ? networkMapping[String(chainId)]["TokenFarm"][0] : constants.AddressZero
+    // approve
+    const tokenFarmInterface = new utils.Interface(abi)
+    const tokenFarmContract = new Contract(tokenFarmAddress, tokenFarmInterface)
+
+    const erc20ABI = ERC20.abi
+    const erc20Interface = new utils.Interface(erc20ABI)
+    const erc20Contract = new Contract(tokenAddress, erc20Interface)
+    // approve
+    const { send: approveErc20Send, state: approveErc20State } =
+        useContractFunction(erc20Contract, "approve", {
+            transactionName: "Approve ERC20 transfer",
+        })
+    const approveAndStake = (amount: string) => {
+        setAmountToStake(amount)
+        return approveErc20Send(tokenFarmAddress, amount)
+    }
+
+    const {send: stakeSend, state: stakeState} = useContractFunction(tokenFarmContract, "stakeTokens", {transactionName: "Stake Tokens"})
+
+    const [amountToStake, setAmountToStake] = useState("0")
+
+    //useEffect
+    useEffect(() => {
+        if(approveErc20State.status === "Success"){
+            // stake function
+            stakeSend(amountToStake, tokenAddress)
+        }
+    }, [approveErc20State])
+
+    return {approveAndStake, approveErc20State}
+
+}
